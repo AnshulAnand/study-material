ff6989aae7f873719bdd0c19d229e1c02f1e9d44
Anshul Anand <anandanshul001@gmail.com>
2024-08-05 22:47:54 +0530

    feat: refactor

--- public/js/release-notes.js
+++ public/js/release-notes.js
@@ -1,30 +1,12 @@
-arr = [
-  '1 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero, qui.',
-  '2 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero, qui. Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-  '3 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero, qui.',
-  '4 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero, qui. Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-  '5 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero, qui.',
-]
+arr = ['Start of our platform, gpamaster.in']
 
-i = 0
-li = document.getElementById('link')
+let i = 0,
+  li = document.getElementById('link')
 
 function myFunction() {
   li.innerText = arr[i]
-  if (i == 4) i = 0
+  if (i == arr.length - 1) i = 0
   else i += 1
 }
 
-// ul = document.getElementById('links')
-
-// function myFunction() {
-//   for (let i = 0; i < arr.length; i++) {
-//     if (ul.firstChild) ul.removeChild(ul.lastChild)
-//     li = document.createElement('li')
-//     li.innerText = arr[i]
-//     ul.appendChild(li)
-//   }
-//   arr.push(arr.shift())
-// }
-
-setInterval(myFunction, 5000)
+setInterval(myFunction, 3500)
